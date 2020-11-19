ee2159839be8995d9e07b9bfd6dd964f3bfd73d3
Shreyash Sharma <sharmashreyash.16@gmail.com>
2020-11-19 13:32:11 -0500

    Commit_01
    Unit Test Class for BANK_ATM Project

---
+++ TestFunction/TestFunction/Customer.cpp
@@ -0,0 +1,95 @@
+#include "Customer.h"
+#include <iostream>
+
+Customer::Customer()
+{
+	arrivalTime = 0;
+	serviceTime = 0;
+	exitTime = 0;
+	//accountMap = { {"PSA",nullptr},{"PCA",nullptr},{"PMM",nullptr},
+	//{"PCD",nullptr}, {"BSA",nullptr}, {"BCA",nullptr}, 
+	//{"BHVC",nullptr}, {"BFC",nullptr} };
+}
+
+Customer::Customer(int aTime, int sTime)
+{
+	arrivalTime = aTime;
+	serviceTime = sTime;
+}
+
+void Customer::addAccount(string accountType, Account* accountObj)
+{
+	// Please follow this nomanclature for input string accountType
+	// For Personal Savings - "PSA"
+	// For Personal Checking - "PCA"
+	// For Personal Money Market - "PMM"
+	// For Personal Certifiation of Deposit - "PCD"
+	// For Business Savings - "BSA"
+	// For Business Checking - "BCA"
+	// For Business High Volume Checking - "BHVC"
+	// For Business Foreign Currency - "BFC"
+
+	accountMap.insert({ accountType, accountObj });
+
+}
+
+Account* Customer::getAccount(string accountType)
+{
+	// Please follow this nomanclature for input string accountType
+	// For Personal Savings - "PSA"
+	// For Personal Checking - "PCA"
+	// For Personal Money Market - "PMM"
+	// For Personal Certifiation of Deposit - "PCD"
+	// For Business Savings - "BSA"
+	// For Business Checking - "BCA"
+	// For Business High Volume Checking - "BHVC"
+	// For Business Foreign Currency - "BFC"
+	map<string, Account*>::iterator it;
+	it = accountMap.find(accountType);
+	Account* accountReturn = nullptr;
+	if (accountMap.find(accountType) != accountMap.end()) {
+		accountReturn = it->second;
+	}
+	return accountReturn;
+}
+
+/*
+void Customer::setarrivalTime(int aTime)
+{
+}*/
+
+int Customer::getarrivalTime()
+{
+	return arrivalTime;
+}
+/*
+void Customer::setserviceTime(int sTime)
+{
+}*/
+
+int Customer::getserviceTime()
+{
+	return serviceTime;
+}
+
+void Customer::setexitTime(int eTime)
+{
+	exitTime = eTime;
+}
+
+int Customer::getexitTime()
+{
+	return exitTime;
+}
+
+
+void Customer::displayAccounts()
+{
+	cout << "Available accounts are:" << endl;
+	for (auto const& x : accountMap)
+	{
+		cout << "Account: " << x.first << endl;
+		cout << "Pointer: "<< x.second << endl;
+	}
+
+}
